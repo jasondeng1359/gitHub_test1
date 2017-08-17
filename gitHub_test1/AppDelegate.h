162c03be4951d61931f0f70ed5916e74a1abf4be
jasondeng1359 <dly2437632511@163.com>
2017-08-18 00:34:11 +0800

    first commit
    just copy files from one folder to this repository

---
+++ gitHub_test1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitHub_test1
+//
+//  Created by Apple on 2017/8/18.
+//  Copyright © 2017年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitHub_test1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitHub_test1
+//
+//  Created by Apple on 2017/8/18.
+//  Copyright © 2017年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
